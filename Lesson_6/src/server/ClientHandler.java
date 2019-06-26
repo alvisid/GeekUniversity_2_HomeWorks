4855de63f947da036e3f4c9b30571f729b86568d
Павел Хапёрский <kayan3000@yandex.ru>
2019-06-26 11:17:15 +0300

    Подправил проект.

---
+++ Lesson_6/src/server/ClientHandler.java
@@ -0,0 +1,98 @@
+package src.server;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Socket socket;
+    private MainServer server;
+    private String nick;
+
+    public ClientHandler(Socket socket, MainServer server) {
+        try {
+            this.socket = socket;
+            this.server = server;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+
+                    try {
+
+                        //authorization
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.startsWith("/auth")) {
+                                String[] tokens = str.split(" ");
+                                String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                                if (newNick != null) {
+                                    sendMsg("/authok");
+                                    nick = newNick;
+                                    server.subscribe(ClientHandler.this);
+                                    break;
+                                } else {
+                                    sendMsg("Неверный логин/пароль.");
+                                }
+                            }
+                        }
+                        //send message
+                        while (true) {
+                            String str = in.readUTF();
+                            if (str.equals("/end")) {
+                                out.writeUTF("/serverClosed");
+                                break;
+                            }
+                            if (str.startsWith("/w")) {
+                                String[] tokens = str.split(" ", 3);
+                                server.sendPersonalMsg(ClientHandler.this, tokens[1], tokens[2]);
+                            }
+                            server.broadCastMsg("Client: " + str);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    server.unsubscribe(ClientHandler.this);
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNick() {
+        return nick;
+    }
+}
